9ca9ef98050eb1b37b51661cf7ff843ef84e95fc
Navezm <navez.martin@gmail.com>
2021-01-21 12:01:51 +0100

    blank

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React, {Component} from 'react';
+import Manga from './components/Manga';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Manga />
+      </div>
+    );
+  };
 };
 
 export default App;
