6c3ff509b48a8a4c59f189854add231565128028
Navezm <navez.martin@gmail.com>
2021-01-21 12:03:28 +0100

    blank

---
+++ src/components/Manga.js
@@ -0,0 +1,13 @@
+import React, {Component, Fragment} from 'react';
+
+class Manga extends Component {
+    render(){
+        return(
+            <Fragment>
+                <h1 id="h1_de_base">Manga</h1>
+            </Fragment>
+        )
+    };
+};
+
+export default Manga;
